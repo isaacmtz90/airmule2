1f692716cfd936f50ccda3b804c09a35f2dc4627
lguifarro <luis_guifarro@yahoo.com>
2013-11-03 00:55:42 -0600

    Insert Test

---
+++ Test.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+<form action="scripts/insert_trips.php" method="post">
+FromCity: <input type="text" name="from_city">
+FromCountry: <input type="text" name="from_country">
+ToCity: <input type="text" name="to_city">
+ToCuntry: <input type="text" name="to_country">
+from_zipcode: <input type="text" name="from_zipcode">
+to_zipcode: <input type="text" name="to_zipcode">
+username: <input type="text" name="username">
+to_when: <input type="text" name="to_when">
+from_when: <input type="text" name="from_when">
+Flight_Number: <input type="text" name="flight_number">
+Flight_Airline: <input type="text" name="flight_airline">
+luggage: <input type="text" name="luggage_space">
+<input type="submit">
+</form>
+
+</body>
+</html>
